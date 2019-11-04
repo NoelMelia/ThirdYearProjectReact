dcb2c47f644cebf225caf4dd80fa9b8190cf0a07
nomelia <noelmelia1989@yahoo.co.uk>
2019-11-04 13:29:39 +0000

    first commit

---
+++ src/components/edit.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import axios from 'axios';
+
+class Edit extends React.Component {
+
+    render(){
+        return(
+            <div className="App">
+            <h1>My Edit component</h1>
+          </div> 
+        )
+    }
+//   constructor(props) {
+//     super(props);
+
+//     this.state = {
+//       Title: '',
+//       Year: '',
+//       Poster: ''
+//     };
+
+//     this.handleSubmit = this.handleSubmit.bind(this);
+//     this.handleMovieTitleChange = this.handleMovieTitleChange.bind(this);
+//     this.handleMovieYearChange = this.handleMovieYearChange.bind(this);
+//     this.handleMoviePosterChange = this.handleMoviePosterChange.bind(this);
+//   }
+}
+export default Edit;
--- src/components/movieitem.js
+++ src/components/movieitem.js
@@ -1,6 +1,23 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import axios from 'axios';
+import {Link} from 'react-router-dom';
 class MovieItem extends React.Component{
+
+  constructor(){
+    super();
+    this.DeleteMovie = this.DeleteMovie.bind(this);
+  }
+  DeleteMovie(e){
+    e.preventDefault();
+    
+
+      alert(this.props.movie._id);
+      axios.delete('http://localhost:4000/api/movies/'+this.props.movie._id)
+      .then()
+      .catch();
+  }
 
     render(){
         return(
@@ -11,16 +28,18 @@ class MovieItem extends React.Component{
 
 
                 <Card  border="primary" style={{ width: '28rem' }}>
-  <Card.Header>{this.props.movie.Title}</Card.Header>
-  <Card.Body>
-    <blockquote className="blockquote mb-0">
-    <img src={this.props.movie.Poster}></img>
-      <footer>
-      {this.props.movie.Year}
-      </footer>
-    </blockquote>
-  </Card.Body>
-</Card>
+                <Card.Header>{this.props.movie.title}</Card.Header>
+                <Card.Body>
+                  <blockquote className="blockquote mb-0">
+                  <img src={this.props.movie.poster}></img>
+                    <footer>
+                    {this.props.movie.year}
+                    </footer>
+                  </blockquote>
+                </Card.Body>
+                <Button variant='danger' onClick={this.DeleteMovie}>Delete</Button>
+                <Link to={"/edit/"+this.props.movie._id} className='btn btn-primary'>Edit</Link>
+              </Card>
             </div>
         )
     }
